5be8cc8251cfcce3d6f0fcc1a117295be8ea9e32
tania <taniaosano15@gmail.com>
2022-08-18 00:06:18 +0300

    Completed Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1* num2;
+console.log("multiply")
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log("random")
+
+const num3 = 24;
+const num4 = 5;
+let mod = num3 % num4;
+console.log("mod")
+
+let max = Math.max(12, 9, 20, 4);
+console.log("max")
